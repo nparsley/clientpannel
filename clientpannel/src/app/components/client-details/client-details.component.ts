42200c79afd8828c85108ca397e224e243afe2d5
nparsley <nickparsley5@gmail.com>
2020-12-17 19:04:18 -0700

    client balance update

--- clientpannel/src/app/components/client-details/client-details.component.ts
+++ clientpannel/src/app/components/client-details/client-details.component.ts
@@ -37,4 +37,16 @@ export class ClientDetailsComponent implements OnInit {
     });
   }
 
+  updateBalance() {
+      this.clientService.updateClient(this.client);
+      this.flashMessage.show('Balance updated', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+    }
+
+
+  onDeleteClick() {
+
+  }
+
 }
--- clientpannel/src/app/services/client.service.ts
+++ clientpannel/src/app/services/client.service.ts
@@ -49,6 +49,10 @@ export class ClientService {
     return this.client;
   }
 
+  updateClient(client: Client) {
+    this.clientDoc = this.afs.doc(`clients/${client.id}`);
+    this.clientDoc.update(client);
+  }
 
 
 }
