26d758cd41f043259e1e5eb45745899672d8a76d
nparsley <nickparsley5@gmail.com>
2020-12-17 19:31:05 -0700

    edit + client delete

--- clientpannel/src/app/services/client.service.ts
+++ clientpannel/src/app/services/client.service.ts
@@ -54,5 +54,10 @@ export class ClientService {
     this.clientDoc.update(client);
   }
 
+  deleteClient(client: Client) {
+    this.clientDoc = this.afs.doc(`clients/${client.id}`);
+    this.clientDoc.delete();
+  }
+
 
 }
